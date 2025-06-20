9f504de1e5ebadd39b2ff97da011163cc4c3f507
khalarlay21501 <C250324@ccg.ac.jp>
2025-06-21 01:03:33 +0900

    bla

--- controllers/omikuji_controller.py
+++ controllers/omikuji_controller.py
@@ -2,7 +2,7 @@ import random
 import pandas as pd
 class OmikujiController:
     def __init__(self):
-        self.data = pd.read_csv("/Users/khein21502/Documents/中間/app/data/omikuji_fortune.csv")
+        self.data = pd.read_csv("/Users/khein21502/Documents/中間/project1/data/omikuji_fortune.csv")
         self.omikuji_list = self.data["運勢"].tolist()
         self.result = random.choice(self.omikuji_list)
     def draw(self):
